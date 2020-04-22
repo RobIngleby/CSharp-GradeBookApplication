78d705c0984074bda655fea2f73fb007b506c55d
RobIngleby <robert.ingleby@mail.dk>
2020-04-22 18:51:33 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,9 +7,10 @@ namespace GradeBook.GradeBooks
     public  class RankedGradeBook:BaseGradeBook
     {
 
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
    
 
         public override char GetLetterGrade(double averageGrade)
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -9,7 +9,8 @@ namespace GradeBook.GradeBooks
     public class StandardGradeBook :BaseGradeBook
     {
         // call to base ctor passing name.
-        public StandardGradeBook(string name):base(name)         {
+        public StandardGradeBook(string name):base(name)     
+        {
 
            Type = GradeBookType.Standard;
         }
