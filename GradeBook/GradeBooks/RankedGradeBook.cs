4b3493f23f62a67a469fcd4955764a767d6eedf8
RobIngleby <robert.ingleby@mail.dk>
2020-04-22 18:27:49 +0200

    Completed module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,35 @@
+﻿using GradeBook.Enums;
+using System;
+
+namespace GradeBook.GradeBooks
+{
+    public  class RankedGradeBook:BaseGradeBook
+    {
+
+        public RankedGradeBook(string name):base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count > 5)
+            {
+                throw new InvalidOperationException();
+
+            }
+            return 'F'; // base.GetLetterGrade(averageGrade);
+        }
+
+
+    }
+}
+
+//In the RankedGradeBook class create an override for the GetLetterGrade method.
+
+//The GetLetterGrade method returns a char and accepts a double named "averageGrade".
+
+//If there are less than 5 students throw an InvalidOperationException. (Ranked-grading requires a minimum of 5 students to work)
+
+//Return 'F' at the end of the method.
