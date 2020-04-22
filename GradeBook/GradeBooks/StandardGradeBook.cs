0fb0c933bf25c04bec8dd4aa24758fde8e0555b7
RobIngleby <robert.ingleby@mail.dk>
2020-04-22 18:01:59 +0200

    Completed module

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook :BaseGradeBook
+    {
+        // call to base ctor passing name.
+        public StandardGradeBook(string name):base(name)         {
+
+           Type = GradeBookType.Standard;
+        }
+    }
+}
