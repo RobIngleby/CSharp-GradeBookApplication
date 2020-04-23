c48e19bdacbcf0cbc625f0f42cb22c1898663667
RobIngleby <robert.ingleby@mail.dk>
2020-04-23 14:55:04 +0200

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -47,7 +47,7 @@ namespace GradeBook.UserInterfaces
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            if (type == "ranked")
+            else if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
